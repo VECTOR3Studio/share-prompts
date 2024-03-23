479cb8699e4ff7975df95b1377c7d40f66913313
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2024-03-23 21:24:01 +0100

    "Update Nav.jsx: import signOut, fix providers state, and add conditional rendering for sign in/out"
    This commit updates the Nav.jsx component by:
    
    1. Importing `signOut` instead of `SignOut` from 'next-auth/react'.
    2. Adding a state variable `providers` and initializing it with a null value.
    3. Using `useEffect` to fetch the providers and update the state.
    4. Adding a conditional rendering for sign in/out based on the `isUserLoggedIn` variable.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,8 +2,19 @@
 import Link from "next/link"
 import Image from "next/image"
 import { useState, useEffect } from "react"
-import {signIn, SignOut, useSession, getProviders } from 'next-auth/react'
+import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
 const Nav = () => {
+
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null)
+  useEffect(() => {
+    const setProviders = async () =>{
+      const response = await getProviders()
+      setProviders(response)
+    }
+    setProviders();
+  },[])
+  
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -12,7 +23,25 @@ const Nav = () => {
       </Link>
 
       <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
 
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image src="assets/images/logo.svg" width={37} height={37} alt="Profile Icon"/>
+            </Link>
+
+          </div>
+        ):(
+          <>
+
+          </>
+        )}
       </div>
     </nav>
   )
