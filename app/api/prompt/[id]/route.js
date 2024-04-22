f099ee448fb311b00dd7e2197589989126e272f6
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2024-04-22 22:05:36 +0200

    "Add PATCH request handler for updating a prompt with new prompt and tag"
    This commit adds a new PATCH request handler to the `/app/api/prompt/[id]/route.js` file. The handler extract

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -20,4 +20,20 @@ export const GET = async (request, { params }) => {
 
 export const PATCH = async (request, { params }) => {
     const { prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!prompt) return new Response("Prompt not found", {status: 404});
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt), {status: 200})
+    } catch (error) {
+        return new Response("Failed to update prompt", { status: 500})
+    }
 }
