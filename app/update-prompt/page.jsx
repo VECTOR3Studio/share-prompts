33291f3bfa510b7cca77d3202a116b7e635723cf
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2024-05-05 21:53:51 +0200

    Add Suspense for Form component in UpdatePrompt page
    This commit adds the `Suspense` component from React to wrap the `Form` component in `UpdatePrompt` page. The fallback prop is set to a

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from 'react';
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -53,6 +54,7 @@ const UpdatePrompt = () => {
   };
 
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <Form
       type='Edit'
       post={post}
@@ -60,6 +62,7 @@ const UpdatePrompt = () => {
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
+    </Suspense>
   );
 };
 
